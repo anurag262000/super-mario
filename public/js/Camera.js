f64216441af8a785a17aabcf0c9a1af638f44041
anurag262000 <anuragmishra262000@gmail.com>
2024-03-12 15:17:21 +0530

    added-enimes and animation

--- public/js/Camera.js
+++ public/js/Camera.js
@@ -3,7 +3,6 @@ import { Vec2 } from './math.js';
 export default class Camera {
     constructor() {
         this.pos = new Vec2(0, 0);
-        this.size = new Vec2(640, 240); // Adjusted to match canvas size
+        this.size = new Vec2(640, 224);
     }
 }
-
